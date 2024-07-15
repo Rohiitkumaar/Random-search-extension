d09ae9e9adfddf78f0030549ef14dbdd42948130
Rohiitkumaar <rohitkumarverma289@gmail.com>
2024-07-15 11:07:55 +0530

    array updated
    unique word array

--- pageLoad.js
+++ pageLoad.js
@@ -1,18 +1,3 @@
-// pages = [
-//     "https://www.bing.com/search?q=man&form=QBLH&sp=-1&ghc=1&lq=0&pq=man&sc=11-3&qs=n&sk=&cvid=17487D35C4514CB69F2933474E2ECBEC&ghsh=0&ghacc=0&ghpl=",
-//     "https://www.bing.com/search?q=Hen&form=QBLH&sp=-1&ghc=1&lq=0&pq=man&sc=11-3&qs=n&sk=&cvid=17487D35C4514CB69F2933474E2ECBEC&ghsh=0&ghacc=0&ghpl=",
-//     "https://www.bing.com/search?q=Span&form=QBLH&sp=-1&ghc=1&lq=0&pq=man&sc=11-3&qs=n&sk=&cvid=17487D35C4514CB69F2933474E2ECBEC&ghsh=0&ghacc=0&ghpl=",
-//     "https://www.bing.com/search?q=Pink&form=QBLH&sp=-1&ghc=1&lq=0&pq=man&sc=11-3&qs=n&sk=&cvid=17487D35C4514CB69F2933474E2ECBEC&ghsh=0&ghacc=0&ghpl=",
-//     "https://www.bing.com/search?q=Power&form=QBLH&sp=-1&ghc=1&lq=0&pq=man&sc=11-3&qs=n&sk=&cvid=17487D35C4514CB69F2933474E2ECBEC&ghsh=0&ghacc=0&ghpl=",
-//     "https://www.bing.com/search?q=Super+Hero&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq=super+hero&sc=11-10&sk=&cvid=C9695BD464154094815CEB4D5124B87A&ghsh=0&ghacc=0&ghpl="
-
-// ]
-
-// const random = Math.floor(Math.random() * pages.length);
-// console.log(random, pages[random]);
-
-// chrome.tabs.update({url: pages[random]});
-
 
 // Template URL with "man" to be replaced
 const templateURL = "https://www.bing.com/search?q=man&form=QBLH&sp=-1&ghc=1&lq=0&pq=man&sc=11-3&qs=n&sk=&cvid=17487D35C4514CB69F2933474E2ECBEC&ghsh=0&ghacc=0&ghpl=";
@@ -23,39 +8,40 @@ let usedWords = [];
 // Function to generate a random word
 function generateRandomWord() {
   const words = [
-    "cat", "dog", "bird", "tree", "house", "car", "banana", "apple", "orange", 
-    "computer", "book", "pen", "chair", "table", "phone", "flower", "mountain", 
-    "river", "ocean", "sun", "moon", "star", "cloud", "sky", "fish", "lion", 
-    "tiger", "elephant", "zebra", "giraffe", "monkey", "snake", "turtle", "frog", 
-    "butterfly", "bee", "ant", "spider", "snail", "duck", "goose", "swan", "eagle", 
-    "owl", "penguin", "bear", "wolf", "fox", "deer", "rabbit", "squirrel", "hamster", 
-    "mouse", "rat", "guinea pig", "pig", "cow", "horse", "sheep", "goat", "chicken", 
-    "duck", "turkey", "peacock", "parrot", "pigeon", "rooster", "dolphin", "whale", 
-    "shark", "octopus", "jellyfish", "starfish", "coral", "crab", "lobster", "seagull", 
-    "pelican", "albatross", "hummingbird", "woodpecker", "sparrow", "bluebird", "robin", 
-    "blue jay", "cardinal", "hawk", "falcon", "vulture", "bat", "moth", "caterpillar",
-    "unicorn", "dragon", "phoenix", "mermaid", "centaur", "werewolf", "vampire", 
-    "witch", "wizard", "fairy", "gnome", "elf", "dwarf", "goblin", "troll", 
-    "sphinx", "kraken", "yeti", "ogre", "leprechaun", "pegasus", "griffin", 
-    "minotaur", "hydra", "chimera", "siren", "cyclops", "medusa", "sasquatch", 
-    "abominable snowman", "mothman", "bigfoot", "nessie", "jackalope", "kraken", 
-    "flying saucer", "alien", "robot", "android", "dinosaur", "pterodactyl", 
-    "tyrannosaurus rex", "velociraptor", "triceratops", "stegosaurus", "brontosaurus", 
-    "diplodocus", "pteranodon", "ankylosaurus", "archaeopteryx", "spinosaurus", "mosasaurus",
-    "apple", "ball", "cat", "dog", "elephant", "fish", "guitar", "hat", "ice cream", 
-    "juice", "kite", "lemon", "monkey", "nest", "orange", "pencil", "queen", "robot", 
-    "sun", "tiger", "umbrella", "violin", "watermelon", "xylophone", "yoyo", "zebra",
-    "pizza", "burger", "fries", "sandwich", "salad", "pasta", "soup", "sushi", "taco", 
-    "burrito", "quesadilla", "nachos", "hotdog", "pancake", "waffle", "donut", "cookie", 
-    "cake", "pie", "ice cream", "coffee", "tea", "juice", "soda", "smoothie", "milkshake",
-    "sunflower", "rainbow", "waterfall", "butterfly", "fireworks", "campfire", "bonfire", 
-    "fireplace", "waterfall", "rainforest", "volcano", "earthquake", "tsunami", "tornado", 
-    "hurricane", "blizzard", "avalanche", "thunderstorm", "lightning", "raindrop", "snowflake", 
-    "hailstone", "meteor", "comet", "galaxy", "universe", "nebula", "constellation", "planet", 
-    "moonlight", "starlight", "sunshine", "rainbow", "aurora", "eclipse", "twilight", "dawn", 
-    "dusk", "midnight", "noon", "sunset", "sunrise", "spring", "summer", "autumn", "winter", 
-    "daytime", "nighttime", "morning", "afternoon", "evening", "midday", "midnight", "horizon"
-  ];
+    "watermelon", "blackboard", "frog", "ship", "computer", "vampire", "dusk", "fairy",
+    "lemon", "meteor", "midnight", "spider", "ball", "winter", "lantern", "airplane",
+    "quesadilla", "bicycle", "surfboard", "universe", "deer", "juice", "daytime", "chicken",
+    "fish", "bee", "werewolf", "queen", "drum", "bluebird", "rollerblades", "dog", "turtle",
+    "drone", "dawn", "gnome", "mothman", "dolphin", "goblin", "zebra", "pencil", "twilight",
+    "alien", "milkshake", "satellite", "eagle", "monkey", "snowflake", "bigfoot", "tsunami",
+    "giraffe", "swan", "starfish", "troll", "minotaur", "backpack", "jackalope", "sasquatch",
+    "cat", "donut", "nachos", "abominable snowman", "guitar", "yeti", "stegosaurus", "fireplace",
+    "cow", "campfire", "peacock", "snowboard", "jet", "ocean", "ankylosaurus", "umbrella",
+    "falcon", "griffin", "hotdog", "pterodactyl", "pelican", "kite", "car", "velociraptor",
+    "chair", "wizard", "pancake", "aurora", "burger", "telescope", "astronaut", "evening",
+    "bonfire", "salad", "ferry", "eclipse", "balloon", "sailboat", "hailstone", "sandwich",
+    "siren", "spinosaurus", "cake", "coffee", "volcano", "snake", "constellation", "skates",
+    "rocket", "planet", "midday", "bear", "lion", "squirrel", "guinea pig", "duck", "sunrise",
+    "globe", "rabbit", "hamster", "woodpecker", "octopus", "mountain", "leprechaun", "apple",
+    "blue jay", "pasta", "cardinal", "afternoon", "moth", "canoe", "parachute", "dragon",
+    "owl", "robin", "flying saucer", "yoyo", "motorcycle", "dinghy", "unicorn", "horse", "cruise",
+    "witch", "medusa", "nebula", "microscope", "pen", "pteranodon", "raindrop", "mermaid", "sunset",
+    "xylophone", "tugboat", "moon", "rainbow", "mosasaurus", "pig", "soup", "moonlight", "ogre",
+    "noon", "waffle", "autumn", "snail", "nighttime", "gondola", "spaceship", "kraken", "rooster",
+    "spacesuit", "jellyfish", "tea", "raft", "cookie", "tyrannosaurus rex", "waterfall", "summer",
+    "rat", "phoenix", "smoothie", "hurricane", "penguin", "sunshine", "turkey", "skis", "pie",
+    "vulture", "seagull", "boat", "comet", "whistle", "avalanche", "helicopter", "brontosaurus",
+    "sphinx", "banana", "dwarf", "goat", "thunderstorm", "tree", "sun", "elephant", "wolf", "yacht",
+    "parrot", "pigeon", "river", "star", "chimera", "sky", "centaur", "lobster", "cyclops",
+    "hummingbird", "sparrow", "book", "ice cream", "flower", "whale", "calculator", "hydra", "soda",
+    "galaxy", "starlight", "blizzard", "diplodocus", "albatross", "rowboat", "lightning", "phone",
+    "dinosaur", "house", "fireworks", "caterpillar", "tornado", "rover", "spacecraft", "crab",
+    "morning", "scooter", "earthquake", "android", "cloud", "tiger", "ant", "sheep", "bat",
+    "submarine", "shark", "hat", "fries", "pizza", "sunflower", "burrito", "nessie", "skateboard",
+    "taco", "pegasus", "hawk", "kayak", "sushi", "nest", "triceratops", "goose", "fox", "orange",
+    "archaeopteryx", "bird", "violin", "table", "mouse", "coral", "rainforest", "horizon", "robot",
+    "elf", "butterfly", "spring"
+];
   let randomIndex = Math.floor(Math.random() * words.length);
   let randomWord = words[randomIndex];
   
